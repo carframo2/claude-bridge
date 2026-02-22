286fb30667909ba7cdcd5c6e4461d2d521f38fb3
carframo2 <97231037+carframo2@users.noreply.github.com>
2026-02-22 17:20:26 +0100

    Update cors.py

--- core/cors.py
+++ core/cors.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+def install_cors(app: Flask) -> None:
+    @app.after_request
+    def add_cors_headers(resp):
+        resp.headers["Access-Control-Allow-Origin"] = "*"
+        resp.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+        resp.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, X-BRIDGE-TOKEN, X-UPLOAD-SECRET"
+        resp.headers["Access-Control-Max-Age"] = "86400"
+        return resp
